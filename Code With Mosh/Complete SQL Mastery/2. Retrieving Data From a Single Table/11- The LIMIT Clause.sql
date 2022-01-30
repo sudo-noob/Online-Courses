53d948258762b6036a1cf59877ab8338af501951
Shantanu Mangale <shantanu.mangale@gmail.com>
2022-01-30 07:50:08 +0530

    Practice for 30-01-2022

---
+++ Code With Mosh/Complete SQL Mastery/2. Retrieving Data From a Single Table/11- The LIMIT Clause.sql
@@ -0,0 +1,17 @@
+-- SELECT *
+-- FROM customers
+-- LIMIT 3
+-- page 1: 1 - 3
+-- page 2: 4 - 6
+-- page 3: 7 - 9
+
+
+-- SELECT *
+-- FROM customers
+-- LIMIT 6,3
+
+
+SELECT *
+FROM customers
+ORDER BY points DESC
+LIMIT 3
