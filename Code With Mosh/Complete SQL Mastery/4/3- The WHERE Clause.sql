5d70306c747777d94e9212166858d8e51244103d
Shantanu Mangale <shantanu.mangale@gmail.com>
2022-02-04 23:24:24 +0530

    Practice for 04-02-2022

---
+++ Code With Mosh/Complete SQL Mastery/4/3- The WHERE Clause.sql
@@ -0,0 +1,16 @@
+-- SELECT *
+-- FROM customers
+-- WHERE points > 3000
+
+
+-- SELECT *
+-- FROM customers
+-- WHERE state != 'VA'
+
+-- SELECT *
+-- FROM customers
+-- WHERE birth_date > '1990-01-01'
+
+SELECT *
+FROM orders
+WHERE order_date >= '2019-01-01'
