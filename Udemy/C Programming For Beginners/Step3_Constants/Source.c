3d8ad7b4a1aa3d369d0623ebfb0cfe9023b1c269
Shantanu Mangale <shantanu.mangale@gmail.com>
2022-11-07 07:19:51 +0530

    Practice for 07-11-2022

---
+++ Udemy/C Programming For Beginners/Step3_Constants/Source.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define _USE_MATH_DEFINES
+#include<math.h>
+
+int main(int argc, char** argv)
+{
+#define PI 3.141593
+	printf("%f\n", PI);
+	printf("%.10f\n", M_PI);
+	return 0;
+}
